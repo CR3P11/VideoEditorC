619357a20b0a0271de247f51e1a8ffd43c2a50a2
Crepy <crepyland@gmail.com>
2022-08-07 22:45:12 +0200

    Primer commit

---
+++ EditorDeVideo.cpp
@@ -0,0 +1,32 @@
+#include <iostream.h>
+
+
+class VideoManagerClass{
+
+    //Aqui se pondra todo el codigo para el manejo de video
+
+
+
+}
+
+class AudioManagerClass{
+
+
+    //Aqui se pondra todo el codigo para el manejo de audio
+
+
+
+}
+
+
+
+
+
+
+void main(){
+
+        //Aqui se pondra el codigo principal para ejecutar el programa
+
+
+
+}
